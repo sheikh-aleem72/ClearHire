9e4fe9683add3599433bf92206a8fe4877fbd663
aleem <sheikhaleem363@gmail.com>
2026-08-19 12:21:00 +0530

    fix(backend): Remove legacy code

--- backend/src/routers/v1Routers/cloudinary.routes.ts
+++ backend/src/routers/v1Routers/cloudinary.routes.ts
@@ -1,14 +1,10 @@
 import express from 'express';
 import { authMiddleware } from '../../middleware/authMiddleware';
 import { getPresignedUrls } from '../../controllers/cloudinary.controller';
-import { uploadMultipleMiddleware, uploadMultipleToCloudinary } from '../../scripts/testCloudinary';
 
 const router = express.Router();
 
 // Route to get presigned url to upload resume to cloudinary
 router.post('/presigned-urls', authMiddleware, getPresignedUrls);
-
-// route to directly upload file to cloudinary (For testing)
-router.get('/upload', uploadMultipleMiddleware, uploadMultipleToCloudinary);
 
 export default router;
